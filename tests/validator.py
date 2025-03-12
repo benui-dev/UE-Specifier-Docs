1149bfa0115446530098e42feda8b533e157ec40
benui <ben@benui.ca>
2025-03-11 23:30:37 -0700

    no idea

--- tests/validator.py
+++ tests/validator.py
@@ -5,7 +5,62 @@ from yaml.loader import SafeLoader
 
 import os
 os.system('color')
-import pathlib
+from pathlib import Path
+from glob import glob
+
+import re
+
+starts_with_uppercase_regex = re.compile('^[A-Z]')
+
+glob_search = f"./**/*.yml"
+all_files = glob(glob_search, recursive=True)
+
+class IdLookup(): 
+	def __init__(self, files: list[str]):
+		self.found_stuff: map[str,set[str]] = {}
+
+		for file in files:
+			if file.endswith('.yml'):
+				with open(file, encoding='utf8') as f:
+					data = yaml.load(f, Loader=SafeLoader)
+					if 'id' in data:
+						id = data['id'].lower()
+						if id in self.found_stuff:
+							print("Non-unique id")
+						self.found_stuff[id] = set()
+
+						# gather ids
+						if 'specifiers' in data:
+							# gather ids
+							for entry in data['specifiers']:
+								if 'name' in entry:
+									property_name = entry['name'].lower()
+									if property_name in self.found_stuff[id]:
+										print(f"Duplicate property name {property_name}")
+									self.found_stuff[id].add(property_name)
+									if not property_name in self.found_stuff[id]:
+										print("NOT FOUND")
+
+	def has_key(self, raw_key: str, current_page: str) -> bool:
+		property = raw_key.lower()
+
+		page_to_search = current_page
+		# split into class and property
+		if '.' in property:
+			page, property = property.split('.')
+			page_to_search = page
+
+		if page_to_search in self.found_stuff:
+			if not property in self.found_stuff[page_to_search]:
+				#print(f"Property {raw_key} not found in page {page_to_search}")
+				#print(self.found_stuff[page_to_search])
+				return False
+		else:
+			#print(f"Property {raw_key} refers to unknown page {page_to_search}")
+			return False
+		return True
+
+id_lookup = IdLookup(all_files)
 
 class bcolors:
     HEADER = '\033[95m'
@@ -18,8 +73,6 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-import pathlib
-
 class PropertyValidator:
 	def __init__(self):
 		self.type = 'str'
@@ -29,6 +82,31 @@ class PropertyValidator:
 
 class SpecifierValidatorBase:
 	def __init__(self):
+		self.properties = [
+			{ 'name': 'name', 'required': True, 'datatype': str },
+			{ 'name': 'group', 'required': True, 'datatype': str  },
+			{ 'name': 'position', 'required': True, 'datatype': str },
+			{ 'name': 'type', 'required': True, 'datatype': str },
+			{ 'name': 'group' , 'required': True, 'datatype': str }
+			{ 'name': 'subgroup', 'required': False, 'datatype': str },
+			{ 'name': 'comment', 'required': False, 'datatype': str },
+			{ 'name': 'samples', 'required': False, 'datatype': list[str] },
+			{ 'name': 'type-comment', 'required': False, 'datatype': str },
+			{ 'name': 'documentation', 'required': False, 'datatype': str },
+			{ 'name': 'examples', 'required': False, 'datatype': list[str] },
+			{ 'name': 'images', 'required': False, 'datatype': list[str] },
+			{ 'name': 'keywords', 'required': False, 'datatype': list[str] },
+			{ 'name': 'links', 'required': False, 'datatype': list[str] },
+			{ 'name': 'required', 'required': False, 'datatype': list[str] },
+			{ 'name': 'related', 'required': False, 'datatype': list[str] },
+			{ 'name': 'incompatible', 'required': False, 'datatype': list[str] },
+			{ 'name': 'synonyms', 'required': False, 'datatype': list[str] },
+			{ 'name': 'antonyms', 'required': False, 'datatype': list[str] },
+			{ 'name': 'deprecated', 'required': False, 'datatype': True },
+			{ 'name': 'version', 'required': False, 'datatype': str },
+			{ 'name': 'utility', 'required': False, 'datatype': int }
+		]
+
 		self.required_properties = [
 			'name',
 			'group',
@@ -45,7 +123,6 @@ class SpecifierValidatorBase:
 			'documentation',
 			'examples',
 			'images',
-			'source',
 			'keywords',
 			'links',
 			'required',
@@ -55,6 +132,7 @@ class SpecifierValidatorBase:
 			'antonyms',
 			'deprecated',
 			'version',
+			'utility',
 		]
 		for val in self.required_properties:
 			self.allowed_properties.append(val)
@@ -82,43 +160,42 @@ class SpecifierValidatorBase:
 			'implies',
 		]
 
-		self.ids = []
-
 	def should_run(self, filename: str) -> bool:
 		return False
 
+	# returns the first valid required field so we can have something hopefully unique to refer
+	# to this entry by
 	def get_identifier(self, entry) -> str:
 		for req in self.required_properties:
 			if req in entry:
 				return f"{entry[req]}"
 		return '[unknown]'
 
-	def validate_file(self, data_file):
+	def validate_file(self, data_file: str) -> None:
 		print(f"{bcolors.HEADER}Validating {data_file}{bcolors.ENDC}")
 
 		with open(data_file, encoding='utf8') as f:
 
 			data = yaml.load(f, Loader=SafeLoader)
 
-			total_specifiers = 0
-			with_errors = 0
-			with_warnings = 0
-			total_errors = 0
-			total_warnings = 0
+			total_specifiers: int = 0
+			with_errors: int = 0
+			with_warnings: int = 0
+			total_errors: int = 0
+			total_warnings: int = 0
+			this_page_id = ''
+
+			if not 'id' in data:
+				errors.append(f"Does not have {id} property")
+			else:
+				this_page_id = data['id']
+
 			if 'specifiers' in data:
 				print(f"YAML Loaded, {len(data['specifiers'])} entries to validate")
 
-				# gather ids
-				for entry in data['specifiers']:
-					if 'name' in entry:
-						id = entry['name'].lower()
-						if id in self.ids:
-							print(f"Not unique ID")
-						else:
-							self.ids.append(id)
 
 				for entry in data['specifiers']:
-					errors, warnings = self.validate_entry(entry)
+					errors, warnings = self.validate_entry(entry=entry, this_page_id=this_page_id)
 
 					# Per-entry summary
 					total_errors += len(errors)
@@ -140,14 +217,14 @@ class SpecifierValidatorBase:
 						print("")
 					total_specifiers += 1
 			else:
-				print("Fail")
+				print("Fail, does not have specifiers property")
 			
 			print(f"{total_specifiers} processed, {bcolors.ERROR}{with_errors} with errors{bcolors.ENDC}, {bcolors.WARNING}{with_warnings} with warnings{bcolors.ENDC}")
 		print("\n====================================\n")
 
-	def validate_entry(self, entry):
-		errors = []
-		warnings = []
+	def validate_entry(self, entry, this_page_id: str) -> tuple[list[str], list[str]]:
+		errors: list[str] = []
+		warnings: list[str] = []
 
 		for req in self.required_properties:
 			if not req in entry:
@@ -161,17 +238,15 @@ class SpecifierValidatorBase:
 			if entry['position'] not in self.valid_positions:
 				errors.append(f"Position value is invalid '{entry['position']}'")
 		
+		name_errors, name_warnings = self.validate_name(entry['name'])
+		errors.append(name_errors)
+		warnings.append(name_warnings)
+		
 		for ref_field in self.reference_fields:
 			if ref_field in entry:
 				for ref_value in entry[ref_field]:
-					lowered_id = ref_value.lower()
-
-					# split into class and property
-					if '.' in lowered_id:
-						page, property = lowered_id.split('.')
-
-					if lowered_id not in self.ids:
-						errors.append(f"Field '{ref_field}' referenced non-existant id '{lowered_id}'")
+					if not id_lookup.has_key(ref_value, this_page_id):
+						errors.append(f"Field '{ref_field}' referenced non-existant id '{ref_value}'")
 		
 		for key, value in entry.items():
 			if not key in self.allowed_properties:
@@ -179,8 +254,23 @@ class SpecifierValidatorBase:
 		
 		return errors, warnings
 
-	def validate():
-		pass
+	def validate_name(self, name: str) -> tuple[list[str], list[str]]:
+		errors: list[str] = []
+		warnings: list[str] = []
+
+		if not re.match(starts_with_uppercase_regex, name):
+			warnings.append(f"Field name {name} does not start with an uppercase letter.")
+		
+		return errors, warnings
+
+
+class UDefaultValidator(SpecifierValidatorBase):
+	def __init__(self, filename: str):
+		super().__init__()
+		self.filename = filename
+
+	def should_run(self, filename: str) -> bool:
+		return filename.endswith(self.filename)
 
 
 class UClassValidator(SpecifierValidatorBase):
@@ -188,20 +278,8 @@ class UClassValidator(SpecifierValidatorBase):
 		super().__init__()
 		self.allowed_properties.append('inherited')
 
-	def should_run(self, filename: str):
+	def should_run(self, filename: str) -> bool:
 		return filename.endswith('uclass.yml')
-
-	def validate(self, entry):
-		errors, warnings = super().validate_entry(entry)
-
-
-class UDefaultValidator(SpecifierValidatorBase):
-	def __init__(self, filename):
-		super().__init__()
-		self.filename = filename
-
-	def should_run(self, filename: str):
-		return filename.endswith(self.filename)
 
 
 class UFunctionValidator(SpecifierValidatorBase):
@@ -209,7 +287,7 @@ class UFunctionValidator(SpecifierValidatorBase):
 		super().__init__()
 		self.allowed_properties.append('implies')
 
-	def should_run(self, filename: str):
+	def should_run(self, filename: str) -> bool:
 		return filename.endswith('ufunction.yml')
 
 
@@ -218,13 +296,8 @@ class UPropertyValidator(SpecifierValidatorBase):
 		super().__init__()
 		self.allowed_properties.append('implies')
 
-	def should_run(self, filename: str):
+	def should_run(self, filename: str) -> bool:
 		return filename.endswith('uproperty.yml')
-
-current_directory = pathlib.Path(__file__).parent.resolve()
-
-yaml_files_path = current_directory / '../yaml/'
-all_files = os.listdir(yaml_files_path)
 
 validators = [
 	UDefaultValidator('uenum.yml'),
@@ -240,30 +313,4 @@ validators = [
 for file in all_files:
 	for validator in validators:
 		if validator.should_run(file):
-			validator.validate_file(yaml_files_path / file)
-
-
-# data = None
-# with open(data_file, encoding='utf8') as f:
-# 	data = yaml.load(f, Loader=SafeLoader)
-# 
-# seen_ids = {}
-# total_errors = 0
-# total_warnings = 0
-# 
-# data = data['specifiers']
-# 
-# count = len(data)
-# i = 1
-# for entry in data:
-# 	errors = []
-# 	warnings = []
-# 
-# 
-# print(f"{count} processed")
-# 
-# print(f"{bcolors.ERROR}Errors:{bcolors.ENDC}     - {total_errors}")
-# print(f"{bcolors.WARNING}Warnings:{bcolors.ENDC}   - {total_warnings}")
-# 
-# if total_errors == 0 and total_warnings == 0:
-# 	print(f"{bcolors.OKGREEN}Success!{bcolors.ENDC}")
+			validator.validate_file(file)
